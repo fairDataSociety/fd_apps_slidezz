62d89af7c23a55fd1e8a8ae79fc6d18a47bc100e
Marko Man <darkobas@gmail.com>
2022-09-05 13:16:51 +0200

    move deployment to testnet
    ** also needs ens domain on testnet (connect with goerli to
       https://app.ens.domains/
    ** our bzz link service is available on
       <domain>.link.dev.fairdatasociety.org

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -123,7 +123,7 @@ export default function Login() {
                 </Button>
                 <Text align="center" variant="subtext">
                   Don&apos;t have an account?{" "}
-                  <Link href="https://fairdrive.vercel.app/register" isExternal>
+                  <Link href="https://create.dev.fairdatasociety.org" isExternal>
                     Register
                   </Link>
                 </Text>
